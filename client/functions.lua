7fd40b30e73ac3725b377ea48db7d55ea2d9ea03
Sadicius <124639760+Sadicius@users.noreply.github.com>
2024-11-06 09:43:09 +0100

    Add files via upload

--- client/functions.lua
+++ client/functions.lua
@@ -1,10 +1,9 @@
 function Notify(msg,type,duration)
         TriggerEvent('rNotify:Tip', msg, duration)
-
 end
 
 RegisterNetEvent('j-reputations:client:Notify')
-AddEventHandler('j-reputations:client:Notify', function(msg,type,duration)
+AddEventHandler('j-reputations:client:Notify', function(msg, type, duration)
     print("notify")
     if Config.menuType == 'ox_lib' then
         lib.notify({title = msg, type = type, duration = duration})
